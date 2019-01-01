f4a8a1e55956356a60284b59506abf69b786e995
Luke <trymup@gmail.com>
2019-01-01 01:16:07 +0100

    Změněný neposlušný texty - zbývá změnit TextEdit k příslušným TextView

--- app/src/main/java/com/example/luke/myfirstappagain/MainActivity.java
+++ app/src/main/java/com/example/luke/myfirstappagain/MainActivity.java
@@ -4,18 +4,22 @@ import android.app.FragmentManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.Fragment;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,8 @@ public class MainActivity extends AppCompatActivity{
         setSupportActionBar(toolbar);
 
         drawerLayout = findViewById(R.id.drawer_layout);
+
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
 
         TextView czechName = (TextView)findViewById(R.id.czech_name);
         czechName.setIncludeFontPadding(false);
@@ -42,5 +48,14 @@ public class MainActivity extends AppCompatActivity{
                         return true;
                     }
                 });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return onOptionsItemSelected(item);
     }
 }
